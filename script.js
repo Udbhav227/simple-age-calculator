47b1bb356e9b03e4cafffbd2632d27949ae5425f
Udbhav Ashish <udbhav227@gmail.com>
2024-09-05 13:50:52 +0530

    edit calculate button

--- script.js
+++ script.js
@@ -44,3 +44,11 @@ document.querySelectorAll('input[type="number"]').forEach((input, index, inputs)
     updateOutline();
   });
 });
+
+window.addEventListener("resize", () => {
+  const calcBtn = document.querySelector(".calc-btn");
+  const viewportHeight = window.innerHeight;
+  
+  // Adjust button position when the keyboard appears
+  calcBtn.style.bottom = `${window.innerHeight < viewportHeight ? 50 : 0}px`;
+});
